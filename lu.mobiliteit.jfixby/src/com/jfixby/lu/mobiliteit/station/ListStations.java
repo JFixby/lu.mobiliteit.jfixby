fa1554bea4ebe215bf49e87b0243573ee83feabe
J Fixby <github@jfixby.com>
2017-02-16 14:55:36 +0100

    xxx

--- lu.mobiliteit.jfixby/src/com/jfixby/lu/mobiliteit/station/ListStations.java
+++ lu.mobiliteit.jfixby/src/com/jfixby/lu/mobiliteit/station/ListStations.java
@@ -7,14 +7,14 @@ import com.jfixby.lu.MobiliteitIO;
 import com.jfixby.lu.mobiliteit.geo.GeoLocation;
 import com.jfixby.scarabei.adopted.gdx.json.GoogleGson;
 import com.jfixby.scarabei.api.collections.List;
-import com.jfixby.scarabei.api.desktop.DesktopSetup;
+import com.jfixby.scarabei.api.desktop.ScarabeiDesktop;
 import com.jfixby.scarabei.api.json.Json;
 import com.jfixby.scarabei.api.log.L;
 
 public class ListStations {
 
 	public static void main (final String[] args) throws IOException {
-		DesktopSetup.deploy();
+		ScarabeiDesktop.deploy();
 		Json.installComponent(new GoogleGson());
 
 		final GeoLocation targetPoint = new GeoLocation(6.1577337, 49.6270327, 18);// @49.6270327,6.1577337,18z
